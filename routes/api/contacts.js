197a2bcd1ef0685129b12dd6ff255a6334c80486
Heorhii Chub <h4y6@i.ua>
2023-06-28 16:03:30 +0300

    modified:   app.js 	modified:   routes/api/contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -5,8 +5,12 @@ const contacts = require("../../models/contacts");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const result = await contacts.listContacts();
-  res.json(result);
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+  } catch (error) {
+    res.json(error.message);
+  }
 });
 
 module.exports = router;
