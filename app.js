97339b7ce53fe8a6ae606c68ede2340844f16b11
Heorhii Chub <h4y6@i.ua>
2023-06-28 15:57:26 +0300

    modified:   app.js 	modified:   routes/api/contacts.js

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-
+const contactsRouter = require("./routes/api/contacts");
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
+
+app.use("/api/contacts", contactsRouter);
+
+app.use((res, req) => {
+  res.status(404).json({ message: "Not found" });
+});
 
 module.exports = app;
